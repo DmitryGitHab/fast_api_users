fbb446e710cfb4cd0a682165fc961197f0e2d522
Dmitry <babanov91@gmail.com>
2023-10-25 23:25:14 +0500

    first commit

---
+++ routers/readme.md
@@ -0,0 +1,8 @@
+
+Для установки необходимых зависимостей:
+
+    pip install -r requirements.txt
+
+Для запуска сервиса введите команду в терминале:
+
+    uvicorn main:app --reload
---
+++ services/user.py
@@ -0,0 +1,70 @@
+from models.user import User
+from dto import user
+from sqlalchemy.orm import Session
+
+
+def create_user(data: user.User, db: Session):
+    user = User(name=data.name)
+
+    try:
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+
+    except Exception as e:
+        print(e)
+
+    return user
+
+
+def get_user(id: int, db):
+    return db.query(User).filter(User.id == id).first()
+
+
+def update(data: user.User, db: Session, id: int):
+    user = db.query(User).filter(User.id == id).first()
+    user.name = data.name
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return user
+
+
+def remove(db: Session, id: int):
+    user = db.query(User).filter(User.id == id).delete()
+    return user
+
+#
+# def create_user(data: user.User, db: Session):
+#     user = User(name=data.name)
+#
+#     try:
+#         db.add(user)
+#         db.commit()
+#         db.refresh(user)
+#
+#     except Exception as e:
+#         print(e)
+#
+#     return user
+#
+#
+# def get_user(id: int, db: Session):
+#     return db.query(User), filter(User.id == id).first()
+#
+#
+# def update(data: user.User, db: Session, id: int):
+#     user = db.query(User).filter(User.id == id).first()
+#     user.name = data.name
+#     db.add(user)
+#     db.commit()
+#     db.refresh(user)
+#
+#     return user
+#
+#
+# def remove(db: Session, id: int):
+#     user = db.query(User).filter(User.id == id).delete()
+#     return user
+#
